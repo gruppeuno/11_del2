28226cac14aef27358f5b47837431865d87b73cf
Mia <miadong179@gmail.com>
2020-10-22 22:40:50 +0200

    bankklasse_1

---
+++ src/Game/BankAccount.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class BankAccount {
+
+        private int balance;
+        public BankAccount(int balance,int FieldValue) {
+        }
+        public int newBalance(int balance, int FieldValue) {
+            return balance += FieldValue;
+        }
+
+
+}
