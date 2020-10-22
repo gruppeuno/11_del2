ecdc9ee5745f49b29110d1f9217d5fdd755e9561
Mia <miadong179@gmail.com>
2020-10-22 22:55:08 +0200

    bankklasse_medtest

--- src/Game/BankAccount.java
+++ src/Game/BankAccount.java
@@ -2,12 +2,21 @@ package Game;
 
 public class BankAccount {
 
+        private final Field updateFieldValue = new Field();
+
         private int balance;
-        public BankAccount(int balance,int FieldValue) {
+        public BankAccount(int balance,int fieldValue) {
         }
         public int newBalance(int balance, int FieldValue) {
             return balance += FieldValue;
         }
 
 
+}
+
+class test1 {
+        public static void main(String[] args) {
+                BankAccount b = new BankAccount(1000, 100);
+                System.out.println(b.newBalance(1000,100));
+        }
 }
