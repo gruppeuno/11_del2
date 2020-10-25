d9c196c32e9846a5835f869256befd7ad7323018
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-10-25 23:22:16 +0100

    fixet roll input og forløkke merge fejl

--- src/Game/GameController.java
+++ src/Game/GameController.java
@@ -57,75 +57,72 @@ public class GameController {
         GUI_Car[] myCars = new GUI_Car[playerCreator.getPlayerArrayLength()];
         //laver spillere for GUI
 
-        for (int i = 0; i < playerCreator.getPlayerArrayLength(); i++) {
-            for (int n = 0; n < playerNumber; n++) {
-                myCars[i] = new GUI_Car();
-                myCars[i].setPrimaryColor(myColors[i]);
-                myPlayers[i] = new GUI_Player(playerCreator.playerArray[i].getPlayerName(), 1000, myCars[i]);
-                gui.addPlayer(myPlayers[i]);
-            }
-
-            while (!currentPlayer.getPlayerWin()) {
-                String currentPlayerName = currentPlayer.getPlayerName();
-                do {
-
-                    //loop til afvente spillerens roll commando i consollen
-                    //TODO: edit ind når spillet skal køre med input fra consol
-                    //playerRollInput();
-
-                    //ruller terninger med RaffleCup samt sætter fieldNumber = terningeværdien
-                    //og kalder på setField
-                    cup.roll();
-                    field.setFieldNumber(cup.getDiceValue());
-
-                    gui.setDice(cup.getDie1Value(), cup.getDie2Value());
-
-                    System.out.println(currentPlayerName + " landede på felt " + field.getFieldNumber() + "\n" + currentPlayerName + field.getFieldMSG());
-
-                    //ingsætter terningernes værdi og spilleren hvis tur det er, i gameturn
-                    //som sørger for at der sker det rigtige ud fra hvad terningerne viser
-
-                    currentPlayer.b.updateBalance(field.getFieldValue());
-                    System.out.println(currentPlayerName + " har nu " + currentPlayer.b.getBalance() + "kr på sin bankkonto");
-
-
-                    myPlayers[turnCount].setBalance(currentPlayer.b.getBalance());
-
-
-                    //giver mulighed for køre igennem flere gange hvis man slår dobbelt
-                    //fra CDIO, Vi beholder da vi muligvis skal bruge i CDIO3
-                } while (currentPlayer.getRollAgain());
-
-                //checker om der er fundet en vinder efter turen
-                if (currentPlayer.getPlayerWin()) {
-                    System.out.println("WINNER!!!!! =  " + currentPlayerName);
-                    break;
-                }
-
-                //giver mulighed for at tilføje flere spillere, turn turnCount+1%playerArray.length
-                //giver turen til spiller 1 fra den sidste spiller, eller giver turen videre fra spiller 1 til 2
-                turnCount = (turnCount + 1) % playerNumber;
-                System.out.println("========================================");
-                System.out.println();
-            }
+        for (int i = 0; i < playerNumber; i++) {
+            myCars[i] = new GUI_Car();
+            myCars[i].setPrimaryColor(myColors[i]);
+            myPlayers[i] = new GUI_Player(playerCreator.playerArray[i].getPlayerName(), 1000, myCars[i]);
+            gui.addPlayer(myPlayers[i]);
         }
 
-        private void playerRollInput() {
-            String rollInput;
-
+        while (!currentPlayer.getPlayerWin()) {
+            String currentPlayerName = currentPlayer.getPlayerName();
             do {
-                System.out.println("Det er din tur " + playerCreator.playerArray[turnCount].getPlayerName() + "\nSkriv \"Roll\" og tryk enter for slå med terningerne!");
 
-                rollInput = scan.nextLine();
+                //loop til afvente spillerens roll commando i consollen
+                //TODO: edit ind når spillet skal køre med input fra consol
+                //playerRollInput();
+
+                //ruller terninger med RaffleCup samt sætter fieldNumber = terningeværdien
+                //og kalder på setField
+                cup.roll();
+                field.setFieldNumber(cup.getDiceValue());
+
+                gui.setDice(cup.getDie1Value(), cup.getDie2Value());
+
+                System.out.println(currentPlayerName + " landede på felt " + field.getFieldNumber() + "\n" + currentPlayerName + field.getFieldMSG());
+
+                //ingsætter terningernes værdi og spilleren hvis tur det er, i gameturn
+                //som sørger for at der sker det rigtige ud fra hvad terningerne viser
+
+                currentPlayer.b.updateBalance(field.getFieldValue());
+                System.out.println(currentPlayerName + " har nu " + currentPlayer.b.getBalance() + "kr på sin bankkonto");
+
+
+                myPlayers[turnCount].setBalance(currentPlayer.b.getBalance());
+
+
+                //giver mulighed for køre igennem flere gange hvis man slår dobbelt
+                //fra CDIO, Vi beholder da vi muligvis skal bruge i CDIO3
+            } while (currentPlayer.getRollAgain());
+
+            //checker om der er fundet en vinder efter turen
+            if (currentPlayer.getPlayerWin()) {
+                System.out.println("WINNER!!!!! =  " + currentPlayerName);
+                break;
             }
-            while (!rollInput.toLowerCase().equals("roll"));
 
-            //Lukker scanner hvis der er fundet en vinder
-            if (playerCreator.playerArray[turnCount].getPlayerWin())
-                scan.close();
+            //giver mulighed for at tilføje flere spillere, turn turnCount+1%playerArray.length
+            //giver turen til spiller 1 fra den sidste spiller, eller giver turen videre fra spiller 1 til 2
+            turnCount = (turnCount + 1) % playerNumber;
+            System.out.println("========================================");
+            System.out.println();
         }
 
 
+    }
+    private void playerRollInput() {
+        String rollInput;
+
+        do {
+            System.out.println("Det er din tur " + playerCreator.playerArray[turnCount].getPlayerName() + "\nSkriv \"Roll\" og tryk enter for slå med terningerne!");
+
+            rollInput = scan.nextLine();
+        }
+        while (!rollInput.toLowerCase().equals("roll"));
+
+        //Lukker scanner hvis der er fundet en vinder
+        if (playerCreator.playerArray[turnCount].getPlayerWin())
+            scan.close();
     }
 
 }
