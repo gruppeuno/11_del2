e055113bfe550d1d607ebaecc28682b1843a74a8
Kristoffer <kristoffer@kritix.dk>
2020-10-26 23:58:19 +0100

    Ændrede numberOfPlayers til at returnere en int, så det er nemmere at teste :))

--- src/Test/PlayerCreatorTest.java
+++ src/Test/PlayerCreatorTest.java
@@ -1,7 +1,6 @@
 package Test;
 
 import Game.PlayerCreator;
-import Game.RaffleCup;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,10 +8,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerCreatorTest {
 
     @Test
-    public void NumberOfPlayerTest() {
-        PlayerCreator NPT = new PlayerCreator();
+    public void PlayerNameTest() {
+        PlayerCreator PNT = new PlayerCreator();
 
 
     }
 
+    @Test
+    public void NumberOfPlayerTest() {
+        PlayerCreatorTest NPT = new PlayerCreatorTest();
+        assertTrue(2 <= NPT.numberOfPlayers() && NPT.numberOfPlayers()<= 8);
+    }
+
+    public int numberOfPlayers(){
+        int numberOfPlayers;
+
+        //While loop til at sikre at der er indtastet mellem 2-8 spillere
+        System.out.println("Indtast et antal spillere mellem 2-8");
+        final int MAX = 8;
+        final int MIN = 2;
+        do{
+            numberOfPlayers = (int)(Math.random() * 8) + 1;
+
+            if (numberOfPlayers< MIN || numberOfPlayers> MAX)
+                System.out.println("Ugyldigt antal spillere. Indtast et antal spillere mellem 2-8");
+        }while (numberOfPlayers< MIN || numberOfPlayers> MAX);
+        System.out.println("numberOfPlayers " + numberOfPlayers);
+        System.out.println();
+
+        return numberOfPlayers;
+    }
 }
