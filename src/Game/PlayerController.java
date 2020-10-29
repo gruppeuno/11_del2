4e1e9c873adca859eecd5a0ac8a58939c70de585
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-10-29 12:20:59 +0100

    små rettelser af javadoc og getters og setters

--- src/Game/PlayerController.java
+++ src/Game/PlayerController.java
@@ -104,9 +104,7 @@ public class PlayerController {
         }
     }
 
-    public int getPlayerArrayLength(){
-        return playerArray.length;
-    }
+    public int getPlayerArrayLength(){ return playerArray.length; }
 
 }
 
