656aeb86067320978438acb5b0dee7dbe3e90dfb
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-11-03 18:45:42 +0100

    de første ændringer i Field klassen, opdateret attributter samt construktør for oprettelse

--- src/Test/BalanceTest.java
+++ src/Test/BalanceTest.java
@@ -7,19 +7,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BalanceTest {
 
-    @Test
-    public void NumberOfPlayerTest() {
-    int balance = 1000;
 
-    Dice dice = new Dice();
-    Field game = new Field();
-
-    for (int i = 0; i <= 1000; i++) {
-        dice.roll();
-        game.setField(dice.getDiceValue());
-        balance += game.getFieldValue();
-        System.out.println(balance);
-    }
-        assertTrue(3000 <= balance);
-    }
 }
