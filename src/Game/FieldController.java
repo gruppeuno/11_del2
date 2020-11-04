c692a992e3e4272348102ddf0dc4bd6821b5530f
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-11-04 15:28:03 +0100

    Sidste ændringer i FieldController, prototype klar til at blive pushet til development

--- src/Game/FieldController.java
+++ src/Game/FieldController.java
@@ -26,49 +26,44 @@ public class FieldController {
             new Field("CHANCE", 21,0,false),
             new Field("VANDLANDET", 22,5,true),
             new Field("STRANDPROMENADEN", 23,5,true),
-
     };
 
-    //checker om field er eget
-    public boolean checkFieldOwner(int fieldNumber){
-        if (fields[fieldNumber].getOwnedByPlayer())
-            return true;
-        else return false;
-    }
-
+    //Når en spiller lander på et felt
     public void landOnField(int playerID, int fieldNumber){
         Field field = fields[fieldNumber];
 
         //TODO; playerID skal føre til player og i if statement(hvor der står 100) skal være en reference
         //TODO til spillers balance
 
+        //er feltet en egendom?
         if (field.getIsProperty()){
+
+            //er feltet ejet?
+            //hvis ja, betal rent
             if (field.getOwnedByPlayer())
             payRent(playerID,field);
 
+            //feltet er ikke ejet, køb felt
             else if (!field.getOwnedByPlayer())
             buyField(playerID,field);
         }
+        //gå i fængsel
         else if (field.getName()=="GÅ I FÆNGSEL")
             moveToPrison(playerID,field);
     }
 
-    //mangler referance fra playerID
-    public void buyField(int playerID, Field field){
+    /** mangler referrance fra playerID */
+    private void buyField(int playerID, Field field){
         field.setPlayerID(playerID);
         //TODO: p.b.updateBalance(field.getFieldPrice());
     }
 
-    public void payRent(int playerID, Field field) {
+    private void payRent(int playerID, Field field) {
         //TODO: field.getPlayerID().b.subtractBalance(field.getFieldRent());
         //TODO: p.b.addBalance(field.getFieldRent());
     }
 
-    public void moveToPrison(int playerID, Field field){
-        if (field.getName()=="GÅ I FÆNGSEL"){
-            //TODO: move playerID til fængsel
-
-        }
-
+    private void moveToPrison(int playerID, Field field){
+        //TODO: move playerID til fængsel
     }
 }
