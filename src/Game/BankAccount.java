8d89f51ff8a4c9e87c894218df5a9809e7281cf6
Kristoffer <kristoffer@kritix.dk>
2020-11-05 13:58:01 +0100

    Updated setBalance code to comply with required method action.

--- src/Game/BankAccount.java
+++ src/Game/BankAccount.java
@@ -35,7 +35,8 @@ public class BankAccount {
                 balance -= value;
         }
 
-        public void setBalance() {
+        public void setBalance(int setValue) {
+                balance = setValue;
         }
 
         public int getBalance() {
