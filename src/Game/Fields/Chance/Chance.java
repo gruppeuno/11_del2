ddd1793cf53e22597158092f18ba1c2f47fded25
Kristoffer <kristoffer@kritix.dk>
2020-11-08 18:23:32 +0100

    Lavet en randomize class

--- src/Game/Fields/Chance/Chance.java
+++ src/Game/Fields/Chance/Chance.java
@@ -3,11 +3,43 @@ package Game.Fields.Chance;
 import Game.Fields.Field;
 import Game.Fields.*;
 
+import java.util.Random;
+
 public class Chance extends Field {
 
+    private int[] chanceArray = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+    /**
+     * Todo list:
+     * 1. Der skal laves en funktion der tage alle mulige chance kort og blander dem i en rækkefølge.
+     * Denne rækkefølge er den rækkefølge den skal bruge i spillet. Når 1 kort bliver taget fra toppen af bunken,
+     * så bliver det kort lagt ind i bunken igen. Nede i bunden.
+     * 2. Disse kort skal så have en effekt. Der er allerede blevet lavet en metode som hedder move, men den tager udgangspunkt i terningen.
+     * Vi kan bruge getFieldNumber, og tiløje eller trække nogle Fields fra, og så SetFieldNumber igen.
+     *
+     * Den første er den der kræver mest, og nummer to, hvis nummer 1 er lavet godt, så er nummer to bare insert, og se det virke.
+     *
+     *
+     * @param name
+     * @param fieldNumber
+     * @param msg
+     */
 
     public Chance(String name, int fieldNumber, String msg) {
         super(name, fieldNumber, msg);
+    }
+
+    public void RandomizeChance() {
+
+        Random rand = new Random();
+
+        for (int i = 0; i < chanceArray.length; i++) {
+            int random = rand.nextInt(chanceArray.length);
+            int temp = chanceArray[random];
+            chanceArray[random] = chanceArray[i];
+            chanceArray[i] = temp;
+        }
+
     }
 
 
@@ -15,10 +47,10 @@ public class Chance extends Field {
 
         final int MAX = 20;
 
-        int chance = (int) (Math.random() * MAX) + 1;
+        for (int i = 1; i < chanceArray.length; i++)
 
-        //TODO: Skal laves om, så det er mindre og pænnere. Det her ligner bæ.
-        switch (chance) {
+
+        switch (chanceArray[i]) {
 
             case 1:
                 break;
