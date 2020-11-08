fcfc7d8613a7af8bd5b8ee749da57f2e89400507
Commando Ninja <haugbolg@gmail.com>
2020-11-08 20:29:33 +0100

    merge fikset

--- src/Game/FieldController.java
+++ src/Game/FieldController.java
@@ -1,6 +1,7 @@
 package Game;
 
 import Game.Fields.*;
+import Game.Fields.Chance.Chance;
 
 public class FieldController {
 
@@ -8,25 +9,25 @@ public class FieldController {
             new Start("START",0, "Du landede på Start"),
             new Property("BURGERBAREN", 1,1, "Du landede på burgerbaren","brown"),
             new Property("PIZZARIAET", 2,1, "Du landede på pizzariaet","brown"),
-            new Parking("CHANCE", 3, "Du landede på chancen"),
+            new Chance("CHANCE", 3, "Du landede på chancen"),
             new Property("SLIKBUTIKKEN", 4,1, "Du landede på slikbutikken", "lightBlue"),
             new Property("ISKIOSKEN", 5,1, "Du landede på iskiosken", "lightBlue"),
             new JailVisit("PÅ FÆNGSELSBESØG", 6, "Du landede på fængsels besøg"),
             new Property("MUSEET", 7,2, "Du landede på museet","pink"),
             new Property("BIBLIOTEKET", 8,2, "Du landede på bibioteket","pink"),
-            new Parking("CHANCE", 9, "Du landede på chancen"),
+            new Chance("CHANCE", 9, "Du landede på chancen"),
             new Property("SKATERPARKEN", 10,2, "Du landede på skaterparken","gold"),
             new Property("SWIMMINGPOOLEN", 11,2, "Du landede på swimmingpoolen","gold"),
             new Parking("Parkering", 12, "Du landede på parkering"),
             new Property("SPILLEHALLEN", 13,3, "Du landede på spillehallen","red"),
             new Property("BIOGRAFEN", 14,3, "Du landede på biografen","red"),
-            new Parking("CHANCE", 15, "Du landede på chancen"),
+            new Chance("CHANCE", 15, "Du landede på chancen"),
             new Property("LEGETØJSBUTIKKEN", 16,3, "Du landede på legetøjsbutikken","yellow"),
             new Property("DYREHANDLEN", 17,3, "Du landede på dyrehandlen","yellow"),
             new Jail("GÅ I FÆNGSEL", 18, "Du landede på gå i fængsel"),
             new Property("BOWLINGHALLEN", 19,4, "Du landede på bowlingehallen","green"),
             new Property("ZOO", 20,4, "Du landede på zoo","green"),
-            new Parking("CHANCE", 21, "Du landede på chancen"),
+            new Chance("CHANCE", 21, "Du landede på chancen"),
             new Property("VANDLANDET", 22,5, "Du landede på vandlandet","blue"),
             new Property("STRANDPROMENADEN", 23,5, "Du landede på strandpromenaden","blue")
     };
@@ -84,7 +85,6 @@ public class FieldController {
 
     }
 
-    public void passedStart(Player player){
     //TODO: ikke færdigt, spørg hjælpelærer
     public boolean ownedBySamePlayer(Player player, Property property){
         String owner1 = property.getOwnerName();
