186b1510883a8608974ce742f19a1769b5a1123b
Mia <miadong179@gmail.com>
2020-11-13 12:09:36 +0100

    læse fra relativ rod

--- src/Game/View/Output.java
+++ src/Game/View/Output.java
@@ -1,13 +1,41 @@
 package Game.View;
 
+import java.io.*;
+
 public class Output {
 
-    public void print(String string){
+    public static void main(String[] args) {
+        Output o = new Output();
+        try {
+            o.readFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public void print(String string){
         System.out.print(string);
     }
 
     public void println(String string){
         System.out.println(string);
+    }*/
+
+
+    public void readFromFile() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader("Textfiles/Udskrivninger.txt"));
+
+        String currentLine = reader.readLine();
+
+        while (currentLine != null){
+            System.out.println(currentLine);
+            currentLine = reader.readLine();
+        }
+
+        reader.close();
+
     }
 
-}
+    }
+
