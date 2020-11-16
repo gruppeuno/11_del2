fde252cd44503701982d68615d191aca0eacf2a2
Mia <miadong179@gmail.com>
2020-11-16 12:16:07 +0100

    teste for at terning er tilnærmelsesvis random

--- src/Test/Test/DiceTest.java
+++ src/Test/Test/DiceTest.java
@@ -1,16 +1,42 @@
 package Test;
 
 import Game.Die;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DiceTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void valueDiceTest() {
-        Die DiceTest = new Die();
-        DiceTest.roll();
-        assertTrue(1 <= DiceTest.getDiceValue() && DiceTest.getDiceValue() <= 6);
+        Die diceTest = new Die();
+        diceTest.roll();
+        assertTrue(1 <= diceTest.getDiceValue() && diceTest.getDiceValue() <= 6);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isDieRandom(){
+        int totalDieValues = 0;
+        boolean isRandom = false;
+
+        Die diceTest = new Die();
+
+        for (int testHundredRolls = 0; testHundredRolls < 100; testHundredRolls++){
+            totalDieValues = 0;
+
+            for (int thisRoll = 0; thisRoll < 1000; thisRoll++){
+                diceTest.roll();
+                diceTest.getDiceValue();
+                totalDieValues += diceTest.getDiceValue();
+            }
+            //3% fejlmargin
+            if (totalDieValues < 3605 && totalDieValues > 3395){
+                isRandom = true;
+            } else {
+                isRandom = false;
+            }
+
+            System.out.println(totalDieValues);
+        }
+
+        assertTrue(isRandom);
     }
 }
