76cbd6049a3723d699bfdbfd3b12091864dc21bb
Y <you@example.com>
2020-11-23 16:11:55 +0100

    Junit test's færdig

--- src/Test/Test/ChanceCardControllerTest.java
+++ src/Test/Test/ChanceCardControllerTest.java
@@ -12,32 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ChanceCardControllerTest {
 
     @Test
-    void randomizeChance() {
-
-    }
-
-   // @Test
-   // void testToString() {
-   // }
-
-//    @Test
-//    void chanceCard() { // et specifikt chancekort (ck 7)
-//        Player testplayer = new Player("Testperson");
-//        PlayerController playerController = new PlayerController();
-//        FieldController fieldController = new FieldController();
-//        ChanceCardController chanceCardController = new ChanceCardController();
-//
-//
-//        playerController.setStartBalance();
-//        chanceCardController.chanceCard(testplayer, playerController, fieldController);
-//        chanceCard.();
-//        int actual = testplayer.bankAccount.getBalance();
-//        int expected = 1;
-//
-//        assertEquals(actual, expected);
-//    }
-
-    @Test
     void moveField() {
         Player testplayer = new Player("Testperson");
         ChanceCardController chanceCardController = new ChanceCardController();
@@ -49,14 +23,6 @@ class ChanceCardControllerTest {
 
     }
 
-    @Test
-    void moveFieldPlayerSelect() {
-        Player testplayer = new Player("Testperson");
-        ChanceCardController chanceCardController = new ChanceCardController();
-        int move;
-
-
-    }
     //Test at spilleren rykker til start
     @Test
     void moveToStart() {
@@ -67,34 +33,6 @@ class ChanceCardControllerTest {
         int expected = 0;
         assertEquals(expected, actual);
 
-    }
-
-    @Test
-    void moveSpecificField() {
-    }
-
-    @Test
-    void addBank() {
-        Player testplayer = new Player("Testperson");
-        ChanceCardController chanceCardController = new ChanceCardController();
-        BankAccount bankAccount = new BankAccount();
-
-    }
-
-    @Test
-    void subBank() {
-    }
-
-    @Test
-    void adjustJailCard() {
-    }
-
-    @Test
-    void setJailCardUse() {
-    }
-
-    @Test
-    void getJailCardUse() {
     }
 
     @Test
@@ -110,11 +48,4 @@ class ChanceCardControllerTest {
         int expected = 1;
         assertEquals(actual, expected);
     }
-
-
- /*   @Test
-    void bankFromAll() {
-        bankFromAll();
-        assertTrue(3,bankFromAll());
-    }*/
 }
