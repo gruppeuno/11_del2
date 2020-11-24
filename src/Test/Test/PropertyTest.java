bf0a5d598a76e6cdc158b23221537350dd64328c
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-11-24 11:59:51 +0100

    lille retter

--- src/Test/Test/PropertyTest.java
+++ src/Test/Test/PropertyTest.java
@@ -1,6 +1,8 @@
 package Test;
 
+import Game.FieldController;
 import Game.Fields.Property;
+import Game.PlayerController;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,29 +38,6 @@ class PropertyTest {
         assertEquals(actual, property.getFieldRent());
     }
 
-
-    /**Test af om setDoubleRent bliver kaldt når spiller køber 2. grund af samme farve*/
-
-  //  //test af om rent bliver dobbelt
-  //  @Test
-  //  public void buyPropertySetDoubleRentTest(){
-  //      PlayerController testPlayerController = new PlayerController();
-  //      Property testProperty = new Property("testVej", 2,4,"test","blå");
-  //      Property testProperty1 = new Property("testVej1", 2,4,"test","rød");
-  //      Property testProperty2 = new Property("testVej2", 2,4,"test","gul");
-  //      Property testProperty3 = new Property("testVej3", 2,4,"test","rød");
-//
-  //      //skaber 2 nye spillere i playerarray
-  //      testPlayerController.createPlayers(2);
-//
-  //      testProperty.buyProperty(testPlayerController.getPlayerByName("p0"), testPlayerController);
-  //      testProperty1.buyProperty(testPlayerController.getPlayerByName("p0"), testPlayerController);
-  //      testProperty2.buyProperty(testPlayerController.getPlayerByName("p0"), testPlayerController);
-  //      testProperty3.buyProperty(testPlayerController.getPlayerByName("p0"), testPlayerController);
-//
-  //      int actual = 8;
-  //      assertEquals(actual,testProperty3.getFieldRent());
-  //  }
 
 
 
